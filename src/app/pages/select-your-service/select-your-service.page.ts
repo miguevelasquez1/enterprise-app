fd5081cc308efece712833b31ad0ddc7379ff153
Miguel <veruiz.miguel@gmail.com>
2022-01-14 07:34:13 -0500

    fix conflicts and browserlistrc

--- src/app/pages/select-your-service/select-your-service.page.ts
+++ src/app/pages/select-your-service/select-your-service.page.ts
@@ -1,12 +1,7 @@
 import { Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
-<<<<<<< HEAD
-import { IonCard } from '@ionic/angular';
-import { AuthService } from '../../services/auth.service';
-=======
 
 import { AuthService } from '../../shared/services/auth/auth.service';
 import { IonCard } from '@ionic/angular';
->>>>>>> intento
 
 @Component({
   selector: 'app-select-your-service',
@@ -14,45 +9,24 @@ import { IonCard } from '@ionic/angular';
   styleUrls: ['./select-your-service.page.scss'],
 })
 export class SelectYourServicePage implements OnInit {
-<<<<<<< HEAD
-
-=======
->>>>>>> intento
   public services = [
     'Technical Service',
     'Laundry Service',
     'Cleaning Service',
     'Electrician Service',
     'Babysitting Service',
-<<<<<<< HEAD
-    'Gardener Service'
-=======
     'Gardener Service',
->>>>>>> intento
   ];
 
   public selectedCard = '';
 
   @ViewChild(IonCard) ionCards: QueryList<IonCard>;
 
-<<<<<<< HEAD
-  constructor(
-    public authService: AuthService
-  ) { }
-
-  ngOnInit() {
-  }
-=======
   constructor(public authService: AuthService) {}
 
   ngOnInit() {}
->>>>>>> intento
 
   selectCard(service) {
     this.selectedCard = service;
   }
-<<<<<<< HEAD
-
-=======
->>>>>>> intento
 }
